bd30f076e7eeefc0c970ca686e911b452ee677fa
Salmaan Rizvi <sar228@cornell.edu>
2020-03-22 20:39:32 -0700

    update configs

--- gatsby-config.js
+++ gatsby-config.js
@@ -7,4 +7,4 @@
 module.exports = {
   /* Your site config here */
   plugins: ['gatsby-plugin-root-import']
-}
+};
--- src/pages/index.js
+++ src/pages/index.js
@@ -2,5 +2,4 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 
 import App from 'src/components/App.jsx'
 
-
 export default App;
